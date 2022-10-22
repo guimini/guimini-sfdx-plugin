6ae50f976afbbfc1b77ecb22a28e0736bf26ea14
Gaël Motte <gaelmotte@Gaels-MacBook-Air.local>
2022-10-22 13:56:32 +0200

    🚧 many small changes

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //...require('@salesforce/prettier-config'), could not get it to work from vscode. Failed to resolve local module
+  singleQuote: true,
+  printWidth: 120,
+  endOfLine: 'auto',
+  trailingComma: 'all',
+};
